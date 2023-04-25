dc46c143f80c2bb35ffde49c487defff1344750b
Elma Geurts <elma@koek.nl>
2023-04-25 19:39:48 +0200

    Add parallax effect to hero.

--- home.js
+++ home.js
@@ -1,8 +1,13 @@
 let lastKnownScrollPosition = 0;
+
 const header = document.getElementsByClassName('header')[0];
 const toggleNav = document.getElementsByClassName('toggle-nav')[0];
 const toggleNavIcon = document.querySelector('.toggle-nav i');
 const navMain = document.getElementsByClassName('nav main')[0];
+const hero = document.querySelector('.hero .inner');
+const heroBackground = document.querySelector('.hero .background');
+const heroTitle = document.querySelector('.hero .title');
+const heroButton = document.querySelector('.hero .button');
 
 function makeHeaderActive(scrollPos) {
     if (scrollPos > header.offsetHeight) {
@@ -12,12 +17,21 @@ function makeHeaderActive(scrollPos) {
     }
 }
 
+function addParallaxEffect(scrollY) {
+    hero.style.top = 0.5 * scrollY + 'px';
+    heroBackground.style.transform = 'scale('+ (0.001 * scrollY + 1) +')';
+    heroTitle.style.top = -1 * scrollY + 'px';
+    heroButton.style.top = -.9 * scrollY + 'px';
+}
+
 document.addEventListener('scroll', (event) => {
     lastKnownScrollPosition = window.scrollY;
 
     window.requestAnimationFrame(() => {
         makeHeaderActive(lastKnownScrollPosition);
     });
+
+    addParallaxEffect(lastKnownScrollPosition);
 });
 
 toggleNav.addEventListener('click', (event) => {
